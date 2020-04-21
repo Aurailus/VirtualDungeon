740a65995171990da376c2ec8c8e4e8605012178
Nicole Collings <100Toby1@gmail.com>
2020-04-20 21:18:14 -0700

    Nieve lighting implementation

--- tool.js
+++ tool.js
@@ -1952,10 +1952,131 @@ class TokenMode {
         this.movingTokens = false;
     }
 }
+class LightChunk {
+    constructor(light, x, y) {
+        this.pos = new Vec2(x, y);
+        this.canvas = light.scene.add.renderTexture(
+        // The magic + 2's are to prevent seeing the seams between chunks.
+        x * LightChunk.CHUNK_SIZE * 4, y * LightChunk.CHUNK_SIZE * 4, LightChunk.CHUNK_SIZE, LightChunk.CHUNK_SIZE);
+        this.light = light;
+        this.canvas.setScale(4);
+        this.canvas.setOrigin(0, 0);
+        this.build();
+    }
+    build() {
+        // Reset
+        const reset = new Phaser.GameObjects.Rectangle(this.light.scene, 0, 0, LightChunk.CHUNK_SIZE, LightChunk.CHUNK_SIZE, 0x000000);
+        reset.setDisplayOrigin(0, 0);
+        reset.setOrigin(0, 0);
+        this.canvas.draw(reset);
+        reset.destroy();
+        for (let light of this.light.lightSources) {
+            let lp = new Vec2(light.x - this.pos.x * LightChunk.CHUNK_SIZE, light.y - this.pos.y * LightChunk.CHUNK_SIZE);
+            if ((lp.x + light.radius > 0 || lp.x - light.radius < LightChunk.CHUNK_SIZE) &&
+                (lp.y + light.radius > 0 || lp.y - light.radius < LightChunk.CHUNK_SIZE)) {
+                this.drawRayTracedCircle(light, lp);
+                // let poly = new Phaser.GameObjects.Ellipse(this.light.scene, lp.x, lp.y, light.radius, light.radius, 0xffffff, light.intensity)
+                // poly.setScale(4, 4);
+                // poly.setBlendMode('ERASE');
+                // poly.setDisplayOrigin(0, 0);
+                // poly.setOrigin(0.5, 0.5);
+                // this.canvas.draw(poly);
+            }
+        }
+    }
+    drawRayTracedCircle(light, lp) {
+        console.log('light');
+        let start = new Vec2(Math.floor(light.x / 16), Math.floor(light.y / 16));
+        let points = [];
+        for (let i = 0; i < 288; i++) {
+            let ray = new Vec2(0.5, 0.5);
+            let dir = new Vec2(Math.cos(i * 1.25 * (Math.PI / 180)) / 32, Math.sin(i * 1.25 * (Math.PI / 180)) / 32);
+            let dist = 0;
+            while (this.light.scene.map.getTileset(1 /* wall */, Math.floor(start.x + ray.x), Math.floor(start.y + ray.y)) == -1 &&
+                (dist = Math.sqrt(Math.pow(ray.x, 2) + Math.pow(ray.y, 2))) < light.radius / 16) {
+                ray.x += dir.x;
+                ray.y += dir.y;
+            }
+            ray.x += dir.x * 0.3;
+            ray.y += dir.y * 0.3;
+            ray.x += dir.x * ((light.radius / 16) - dist) * 0.5;
+            ray.y += dir.y * ((light.radius / 16) - dist) * 0.5;
+            points.push(new Vec2(ray.x * 4, ray.y * 4));
+        }
+        let render = new Phaser.GameObjects.RenderTexture(this.light.scene, lp.x - light.radius, lp.y - light.radius, light.radius * 2, light.radius * 2);
+        let gfx = new Phaser.GameObjects.Graphics(this.light.scene, { x: light.radius, y: light.radius });
+        gfx.setScale(4, 4);
+        gfx.fillStyle(0xffffff, light.intensity / 3);
+        gfx.fillPoints(points, true);
+        for (let i = 0; i < 6; i++) {
+            gfx.scaleX += 0.02;
+            gfx.scaleY += 0.02;
+            render.draw(gfx);
+        }
+        let spr = new Phaser.GameObjects.Sprite(this.light.scene, 0, 0, "shader_light_mask");
+        spr.setScale(light.radius / 128, light.radius / 128);
+        spr.setOrigin(0, 0);
+        spr.setBlendMode(Phaser.BlendModes.ERASE);
+        render.draw(spr);
+        render.setBlendMode(Phaser.BlendModes.ERASE);
+        this.canvas.draw(render);
+        render.destroy();
+    }
+}
+LightChunk.CHUNK_SIZE = 512;
+class LightSource {
+    constructor(light, x, y) {
+        this.radius = 32;
+        this.intensity = 1.0;
+        this.light = light;
+        this.x = x;
+        this.y = y;
+    }
+    setRadius(radius) {
+        this.radius = radius;
+    }
+    setIntensity(intensity) {
+        this.intensity = intensity;
+    }
+}
 class Lighting {
-    constructor(...any) {
+    constructor(scene, size) {
+        this.chunks = [];
+        this.lightSources = [];
+        this.scene = scene;
+        this.size = size;
+        for (let i = 0; i < Math.ceil(size.y / (MapChunk.CHUNK_SIZE * 2)); i++) {
+            this.chunks[i] = [];
+            for (let j = 0; j < Math.ceil(size.x / (MapChunk.CHUNK_SIZE * 2)); j++) {
+                this.chunks[i][j] = new LightChunk(this, j, i);
+            }
+        }
+        this.addLightSource(18 * 16, 18 * 16, 12 * 16, 1);
+        this.addLightSource(32 * 16, 32 * 16, 12 * 16, 1);
+        this.addLightSource(14 * 16, 36 * 16, 12 * 16, 1);
     }
     update() {
+        if (this.scene.i.keyPressed('R')) {
+            this.updateAround(0, 0, 1000);
+        }
+    }
+    updateAround(x, y, radius) {
+        const minChunkPos = new Vec2(clamp(Math.floor((x - radius) / LightChunk.CHUNK_SIZE), 0, this.chunks[0].length - 1), clamp(Math.floor((y - radius) / LightChunk.CHUNK_SIZE), 0, this.chunks.length - 1));
+        const maxChunkPos = new Vec2(clamp(Math.ceil((x + radius) / LightChunk.CHUNK_SIZE), 0, this.chunks[0].length - 1), clamp(Math.ceil((y + radius) / LightChunk.CHUNK_SIZE), 0, this.chunks.length - 1));
+        for (let i = minChunkPos.x; i < maxChunkPos.x; i++) {
+            for (let j = minChunkPos.y; j < maxChunkPos.y; j++) {
+                this.chunks[j][i].build();
+            }
+        }
+    }
+    addLightSource(x, y, radius, intensity) {
+        let s = new LightSource(this, x, y);
+        if (radius !== undefined)
+            s.setRadius(radius);
+        if (intensity !== undefined)
+            s.setIntensity(intensity);
+        this.lightSources.push(s);
+        this.updateAround(x, y, radius);
     }
 }
 class MapChunk {
@@ -2620,86 +2741,6 @@ var SmartTiler;
     }
     SmartTiler.floor = floor;
 })(SmartTiler || (SmartTiler = {}));
-// class TilesetCanvas {
-// 	manager: TilesetManager;
-// 	res: number;
-// 	width: number;
-// 	height: number;
-// 	canvas: Phaser.Textures.CanvasTexture;
-// 	indexes: number[] = [];
-// 	indMap: number[] = [];
-// 	pad: number = 2;
-// 	constructor(manager: TilesetManager, res: number, layer: Layer) {
-// 		this.manager = manager;
-// 		this.res = res;
-// 		this.width = Math.floor(1024 / ((this.res + this.pad * 2) * 9));
-// 		this.height = Math.floor(1024 / ((this.res + this.pad * 2) * 7));
-// 		this.canvas = manager.scene.textures.createCanvas("tileset_" + res + 
-// 			(layer == Layer.wall ? "_wall" : layer == Layer.floor ? "_ground" : "_overlay"), 
-// 			this.width * 9 * (this.res + this.pad * 2), this.height * 7 * (this.res + this.pad * 2));
-// 	}
-// 	addTileset(key: string, ind: number) {
-// 		const x = this.indexes.length % this.width;
-// 		const y = Math.floor(this.indexes.length / this.width);
-// 		this.drawTileset(key, x, y);
-// 		this.indMap[ind] = this.indexes.length;
-// 		this.indexes.push(ind);
-// 	}
-// 	getGlobalIndex(tileset: number, tile: number) {
-// 		const lX = tile % 9;
-// 		const lY = Math.floor(tile / 9);
-// 		const gX = tileset % this.width;
-// 		const gY = Math.floor(tileset / this.width);
-// 		const xx = lX + gX * 9;
-// 		const yy = lY + gY * 9;
-// 		return yy * this.width * 9 + xx;
-// 	}
-// 	getLocalIndex(tile: number): number {
-// 		const gX = tile % (this.width * 9);
-// 		const gY = Math.floor(tile / (this.width * 9));
-// 		const lX = gX % 9;
-// 		const lY = gY % 7;
-// 		return lX + lY * 9;
-// 	}
-// 	private drawTileset(key: string, x: number, y: number) {
-// 	// 	let img: HTMLImageElement = this.manager.scene.textures.get(key).getSourceImage() as HTMLImageElement;
-// 	// 	let refCanvas = document.createElement('canvas');
-// 	// 	refCanvas.width = img.width;
-// 	// 	refCanvas.height = img.height;
-// 	// 	refCanvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
-// 	// 	let imageOffX = 9 * (this.res + this.pad * 2) * x;
-// 	// 	let imageOffY = 7 * (this.res + this.pad * 2) * y;
-// 	// 	for (let i = 0; i < 9; i++) {
-// 	// 		for (let j = 0; j < 7; j++) {
-// 	// 			let frame = i + j * 9;
-// 	// 			let tileOffX = imageOffX + i * (this.res + this.pad * 2);
-// 	// 			let tileOffY = imageOffY + j * (this.res + this.pad * 2);
-// 	// 			for (let k = 0; k < this.res + this.pad * 2; k++) {
-// 	// 				for (let l = 0; l < this.pad * 2; l++) {
-// 	// 					let sX = clamp(k - this.pad, 0, this.res - 1);
-// 	// 					let sY = l < this.pad ? 0 : this.res - 1;
-// 	// 					let oY = l < this.pad ? l : l + this.res;
-// 	// 					let pixel = refCanvas.getContext('2d').getImageData(this.res * i + sX, this.res * j + sY, 1, 1).data;
-// 	// 					this.canvas.setPixel(tileOffX + k, tileOffY + oY, pixel[0], pixel[1], pixel[2], pixel[3]);
-// 	// 				}
-// 	// 			}
-// 	// 			for (let k = 0; k < this.pad * 2; k++) {
-// 	// 				for (let l = 0; l < this.res; l++) {
-// 	// 					let sX = k < this.pad ? 0 : this.res - 1;
-// 	// 					let sY = clamp(l, 0, this.res - 1);
-// 	// 					let oX = k < this.pad ? k : k + this.res;
-// 	// 					let pixel = refCanvas.getContext('2d').getImageData(this.res * i + sX, this.res * j + sY, 1, 1).data;
-// 	// 					this.canvas.setPixel(tileOffX + oX, tileOffY + l + this.pad, pixel[0], pixel[1], pixel[2], pixel[3]);
-// 	// 				}
-// 	// 			}
-// 	// 			this.canvas.drawFrame(key, frame, 
-// 	// 				9 * (this.res + this.pad * 2) * x + i * (this.res + this.pad * 2) + this.pad, 
-// 	// 				7 * (this.res + this.pad * 2) * y + j * (this.res + this.pad * 2) + this.pad)
-// 	// 		}
-// 	// 	}
-// 	// 	refCanvas.remove();
-// 	}
-// }
 class TilesetManager {
     constructor(scene) {
         this.currentWallInd = 0;
